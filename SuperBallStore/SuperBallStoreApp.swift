ee0413a612da2b602ea87060b176a33186e3735c
Dmitrii Kim <mitya_kim@icloud.com>
2022-11-16 10:32:00 -0800

    Initial Commit

---
+++ SuperBallStore/SuperBallStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperBallStoreApp.swift
+//  SuperBallStore
+//
+//  Created by Mitya Kim on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SuperBallStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
